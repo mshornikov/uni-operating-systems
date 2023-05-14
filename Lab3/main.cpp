51a978c6aacaecde0dd69b7bccc73a48ae8fe877
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2023-05-14 14:32:21 +0300

    Fixed to build lab3

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,60 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <random>
 #include <thread>
 #include <chrono>
-#include <semaphore>
+#include <cmath>
 
-
-// глобальные экземпляры двоичных семафоров
-// счетчик объектов установлен в ноль
-// объекты в несигнальном состоянии
-std::binary_semaphore
-        smphSignalMainToThread(0),
-        smphSignalThreadToMain(0);
-
-void ThreadProc()
-{
-    // ждем сигнала от main,
-    // пытаясь уменьшить значение семафора
-    smphSignalMainToThread.acquire();
-
-    // этот вызов блокируется до тех пор,
-    // пока счетчик семафора не увеличится в main
-
-    std::cout << "[thread] Got the signal\n"; // ответное сообщение
-
-    // ждем 3 секунды для имитации какой-то работы,
-    // выполняемой потоком
-    using namespace std::literals;
-    std::this_thread::sleep_for(3s);
-
-    std::cout << "[thread] Send the signal\n"; // сообщение
-
-    // сигнализируем обратно в main
-    smphSignalThreadToMain.release();
+int main() {
+    std::cout << "hah" << std::endl;
 }
-
-int main()
-{
-    // создаем какой-то обрабатывающий поток
-    std::thread thrWorker(ThreadProc);
-
-    std::cout << "[main] Send the signal\n"; // сообщение
-
-    // сигнализируем рабочему потоку о начале работы,
-    // увеличивая значение счетчика семафора
-    smphSignalMainToThread.release();
-
-    // ждем, пока рабочий поток не выполнит свою работу,
-    // пытаясь уменьшить значение счетчика семафора
-    smphSignalThreadToMain.acquire();
-
-    std::cout << "[main] Got the signal\n"; // ответное сообщение
-    thrWorker.join();
-}
-/*
-Вывод:
-[main] Send the signal
-[thread] Got the signal
-[thread] Send the signal
-[main] Got the signal
-*/
